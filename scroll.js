4020c8e797e2c8f89af5484174984713c9ec8107
András <andras.kss@outlook.hu>
2024-03-01 09:29:26 +0100

    major impruvements!

--- scroll.js
+++ scroll.js
@@ -1,9 +1,30 @@
-window.addEventListener("scroll", function() {
+document.addEventListener("DOMContentLoaded", function () {
     var navbar = document.querySelector("nav");
-    if (window.scrollY > 0) {
-      navbar.classList.add("shrink");
-    } else {
-      navbar.classList.remove("shrink");
-    }
-  });
-  
+    var checkbox = document.getElementById("check");
+
+    checkbox.addEventListener("change", function () {
+        if (checkbox.checked) {
+            navbar.classList.add("navbar-colored");
+            navbar.classList.remove("navbar-transparent");
+        } else {
+            if (window.scrollY === 0) {
+                navbar.classList.remove("navbar-colored");
+                navbar.classList.add("navbar-transparent");
+            }
+        }
+    });
+
+    window.addEventListener("scroll", function () {
+        if (window.scrollY > 0) {
+            navbar.classList.add("navbar-shrink");
+            navbar.classList.add("navbar-colored");
+            navbar.classList.remove("navbar-transparent");
+        } else {
+            if (!checkbox.checked) {
+                navbar.classList.remove("navbar-shrink");
+                navbar.classList.remove("navbar-colored");
+                navbar.classList.add("navbar-transparent");
+            }
+        }
+    });
+});
